c62f7771a8f002fe12c3927db254f222aacbfa42
selina ann <selinamanilal@gmail.com>
2023-08-24 05:45:16 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_alphabet - print all the alphabet in lowercase
+ */
+void print_alphabet(void)
+{
+	char letter;
+	for(letter = 'a'; ;etter <= 'z';letter++)
+		_putchar(letter);
+	_putchar('\n')
+}
